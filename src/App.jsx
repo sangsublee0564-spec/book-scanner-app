79fc907a4d1caab2f15c39e584f9bea88a1a3120
sangsub <sangsub.lee0564@gmail.com>
2026-07-21 22:25:08 +0900

    fix jsx syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -303,7 +303,7 @@ function App() {
                 <ul className="blog-list">
                   {book.blogPosts.slice(0, 3).map((post, i) => (
                     <li key={i}>
-                        
+                      
                         href={post.link}
                         target="_blank"
                         rel="noreferrer"
